96bdd15108e852938d02937ca2c80b406bfd343b
Christopher Maselli <ChristopherFMaselli@gmail.com>
2020-03-06 10:33:32 -0500

    Made the graphic aspect of the connect 4 game, now instead of just being terminal and matrix based, the game has a visual component to the screen that shows up as a board, and allows viewing of where the piece will fall when clicked

--- Connect4.py
+++ Connect4.py
@@ -1,7 +1,14 @@
 import numpy
+import pygame
+import sys
+import math
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
 
 def create_board():
@@ -30,8 +37,8 @@ def print_board(board):
 
 def winning_move(board, piece):  # Check if 4 connected pieces are together, algorithm not effieicnt, but I am just doing this for Python practice rather than building something hyper-optimized
     # Check Horizontals
-    for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT-3):
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
             if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
                 return True
     # Check Verticals
@@ -43,51 +50,105 @@ def winning_move(board, piece):  # Check if 4 connected pieces are together, alg
     # Check Positively sloped diagonals
     for c in range(COLUMN_COUNT-3):
         for r in range(ROW_COUNT-3):
-            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+1] == piece and board[r+3][c+1] == piece:
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
                 return True
 
     # Check negatively sloped diagonals
     for c in range(COLUMN_COUNT-3):
         for r in range(3, ROW_COUNT):
-            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+1] == piece and board[r-3][c+1] == piece:
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 return True
 
 
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r *
+                                            SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(
+                c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(
+                    c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (int(
+                    c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    pygame.display.update()
+
+
 board = create_board()
-print_board(board)
 game_over = False
 turn = 0
 
+pygame.init()  # Initialize Pygame
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT*SQUARESIZE
+height = (ROW_COUNT+1)*SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("monospace", 75)
+
 while not game_over:
-    # Ask for player 1 input
-    if turn == 0:
-        selected_column = int(input("Player 1 Make your Selection (0-6):"))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-        if is_valid_location(board, selected_column):
-            row = get_next_open_row(board, selected_column)
-            drop_piece(board, row, selected_column, 1)
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(
+                    screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+            else:
+                pygame.draw.circle(
+                    screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
 
-    if winning_move(board, 1):
-        print("PLAYER 2 WINS!!!")
-        game_over = True
-        break
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+            if turn == 0:
+                posx = event.pos[0]
+                selected_column = int(math.floor(posx/SQUARESIZE))
 
-    # print(selection)
-    # print(type(selection))
-    # Ask for player 2 input
-    else:
-        selected_column = int(input("Player 2 Make your Selection (0-6):"))
+                if is_valid_location(board, selected_column):
+                    row = get_next_open_row(board, selected_column)
+                    drop_piece(board, row, selected_column, 1)
+                    draw_board(board)
 
-        if is_valid_location(board, selected_column):
-            row = get_next_open_row(board, selected_column)
-            drop_piece(board, row, selected_column, 2)
+                    if winning_move(board, 1):
+                        label = myfont.render("Player 1 wins!!", 1, RED)
+                        screen.blit(label, (40, 10))
+                        game_over = True
 
-    if winning_move(board, 2):
-        print("PLAYER 2 WINS!!!")
-        game_over = True
-        break
+            else:
+                posx = event.pos[0]
+                selected_column = int(math.floor(posx/SQUARESIZE))
 
-    print_board(board)
+                if is_valid_location(board, selected_column):
+                    row = get_next_open_row(board, selected_column)
+                    drop_piece(board, row, selected_column, 2)
+                    draw_board(board)
 
-    turn += 1
-    turn = turn % 2  # Make it alternate between 0 and 1
+                    if winning_move(board, 2):
+                        label = myfont.render("Player 2 wins!!", 2, YELLOW)
+                        screen.blit(label, (40, 10))
+                        game_over = True
+
+            turn += 1
+            turn = turn % 2  # Make it alternate between 0 and 1
+
+            draw_board(board)
+
+            if game_over:
+                pygame.time.wait(3000)
